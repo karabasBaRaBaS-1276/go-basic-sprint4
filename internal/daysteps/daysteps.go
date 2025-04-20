1ff7a5ec46109b2af202f6baa263d5e7790e4930
karabasBaRaBaS-1276 <152725752+karabasBaRaBaS-1276@users.noreply.github.com>
2025-04-20 12:33:52 +0300

    Спринт 4. Финальное задание (#1)
    * Add daysteps impl
    
    * Task complited
    
    ---------
    
    Co-authored-by: Осенчаков Роман <rongo@yandex.ru>

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,8 +1,18 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
+
+// Неправильный формат данных
+var errorFormateData = errors.New("wrong data format")
 
 const (
 	// Длина одного шага в метрах
@@ -11,10 +21,60 @@ const (
 	mInKm = 1000
 )
 
+// Парсит строку и возвращает её составные части с нужными типами
+// Принимает на вход строку (data) в формате: <кол-во шагов>,<длительность прогулки>
+// Возвращает:
+//   - количество шагов (int)
+//   - длительность прогулки (time.Duration)
+//   - ошибка, если что-то пошло не так. (error)
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	parseData := strings.Split(data, ",")
+	if len(parseData) != 2 {
+		return 0, 0, fmt.Errorf("%w: two comma-separated values are expected", errorFormateData)
+	}
+
+	steps, err := strconv.Atoi(parseData[0])
+	if err != nil {
+		return 0, 0, fmt.Errorf("%w: %w", errorFormateData, err)
+	}
+	if steps <= 0 {
+		return 0, 0, fmt.Errorf("%w: first value in data must be > 0", errorFormateData)
+	}
+
+	duration, err := time.ParseDuration(parseData[1])
+	if err != nil {
+		return 0, 0, fmt.Errorf("%w: %w", errorFormateData, err)
+	}
+	if duration <= 0 {
+		return 0, 0, fmt.Errorf("%w: second value in data must be > 0", errorFormateData)
+	}
+
+	return steps, duration, nil
 }
 
+// Возвращает информацию об активности за день
+// Принимает на вход:
+//
+//	data — строка с данными в формате: <кол-во шагов>,<продолжительность прогулки в формате 3h50m (3 часа 50 минут)>
+//	weight — вес пользователя в килограммах.
+//	height — рост пользователя в метрах.
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	if steps <= 0 {
+		return ""
+	}
+	distanceKm := (float64(steps) * stepLength) / mInKm
+
+	caloriesDay, err := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
+
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, distanceKm, caloriesDay)
 }
